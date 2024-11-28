8d58a85904d44dac6e7305b0690a661643b91fa5
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-11-28 00:09:10 +0000

    deploy: b429ff6297e093385e706dea032f64fe516c7ae1

---
+++ assets/NotFound-DpRuxcvi.js
@@ -0,0 +1,9 @@
+import{G as a,j as t,F as c}from"./index-B3OhisMy.js";function s(e){return a({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM103 135c9.4-9.4 24.6-9.4 33.9 0l23 23 23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-23 23 23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23-23-23c-9.4-9.4-9.4-24.6 0-33.9zm192 0c9.4-9.4 24.6-9.4 33.9 0l23 23 23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-23 23 23 23c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-23-23-23 23c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l23-23-23-23c-9.4-9.4-9.4-24.6 0-33.9z"},child:[]}]})(e)}function i(){return t.jsxs(l,{children:[t.jsx("span",{className:"text-4xl",children:t.jsx(s,{})}),"Not an available page"]})}const l=c.div`
+h-dvh
+flex
+flex-col
+gap-y-4
+items-center
+justify-center
+text-white
+`;export{i as default};
